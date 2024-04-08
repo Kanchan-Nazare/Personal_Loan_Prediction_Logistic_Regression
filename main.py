2dd379b807c3c6c65bf6607448eac1db7a9b5685
Kanchan-Nazare <160421893+Kanchan-Nazare@users.noreply.github.com>
2024-04-08 23:55:53 +0530

    Add files via upload
    Developed a machine learning model for predicting loan eligibility using feature engineering and various classification algorithms for optimal performance deploy throgh Flask Webframe work.

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask,request,render_template
+
+import pickle
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/get_info', methods = ['POST'])
+def model_prediction():
+    data=request.form
+    print(data)
+    load_model=pickle.load(open('Bank_model.pkl','rb'))
+    print(load_model)
+
+
+
+    user_data=[[float(data['ID']),
+                float(data['Age']),
+                float(data['Experience']),
+                float(data['Income']),
+                float(data['ZIP_Code']),
+                float(data['Family']),
+                float(data['CCAvg']),
+                float(data['Education']),
+                float(data['Mortgage'])
+                
+                ]]
+    print(user_data)
+
+    result=load_model.predict(user_data)
+    print(result)
+
+    Eligblity=['Not_Eligible_for_Loan','Eligible_for_Loan']
+
+    print(f"Prediction =  {Eligblity[result[0]]}")
+
+    return Eligblity[result[0]]
+
+app.run(debug=True)
+    
+
+
